8d47b36a1aabce825950b0bfad0f3f7543ae36f4
Chris Flint <flintedc@gmail.com>
2016-07-29 11:26:51 +0100

    initial commmit

---
+++ client/build/bundle.js
@@ -0,0 +1,73 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	
+	window.onload= function(){
+	  console.log('hello world') 
+	}
+	
+	var main = function(){
+	  
+	}
+	
+	
+	
+	
+	// SAMPLE JSON AJAX REQUEST
+	// var request = new XMLHttpRequest();
+	// request.open("GET","/bucketlist");
+	// request.setRequestHeader('Content-Type', 'application/json');
+	// request.onload = function(){
+	//   if (request.status === 200){
+	//     CODE HERE
+	//   }
+	// }.bind(this)
+	// request.send(null);
+
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
---
+++ client/src/app.js
@@ -0,0 +1,27 @@
+state = {
+  view: ''
+  game: ''
+  games:[]
+}
+window.onload= function(){
+  console.log('hello world') 
+  
+}
+
+var main = function(){
+  
+}
+
+
+
+
+// SAMPLE JSON AJAX REQUEST
+// var request = new XMLHttpRequest();
+// request.open("GET","/bucketlist");
+// request.setRequestHeader('Content-Type', 'application/json');
+// request.onload = function(){
+//   if (request.status === 200){
+//     CODE HERE
+//   }
+// }.bind(this)
+// request.send(null);
