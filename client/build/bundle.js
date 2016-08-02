479fe3bb44dd83a06f15ba567f2fcd2551d5dfac
Chris Flint <flintedc@gmail.com>
2016-08-02 09:49:16 +0100

    added additional teams for testing

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -57,45 +57,41 @@
 	  state.game = new Game();
 	  state.view = new View(state.game);
 	  state.game.addTeam("testTeam")
+	  state.game.addTeam("testTeam2")
+	  state.game.addTeam("testTeam3")
+	
 	  state.view.initialise();
 	  state.game.map.initialise()
 	  state.game.createObjective({
-	      clue: "This is where a king might live?",
-	      hints: ["You will hear from it at 1pm", "You can see it from all around Edinburgh", "at the top of the Royal Mile!"],
-	      latLng: {lat: 55.9486, lng: -3.1999},
-	      tolerance: 500,
-	      foundMessage: "Well Done, it was Edinburgh Castle"
-	    })
-	
+	    clue: "This is where a king might live?",
+	    hints: ["You will hear from it at 1pm", "You can see it from all around Edinburgh", "at the top of the Royal Mile!"],
+	    latLng: {lat: 55.9486, lng: -3.1999},
+	    tolerance: 500,
+	    foundMessage: "Well Done, it was Edinburgh Castle"
+	  })
 	  state.game.createObjective({
-	      clue: "This is where a king might rest?",
-	      hints: ["It's near a very old pub...", "Not far from Duddingston", "Take a seat"],
-	      latLng: {lat: 55.9441, lng: -3.1618},
-	      tolerance: 500,
-	      foundMessage: "Well Done, it was Arthurs Seat"
-	    })
-	
+	    clue: "This is where a king might rest?",
+	    hints: ["It's near a very old pub...", "Not far from Duddingston", "Take a seat"],
+	    latLng: {lat: 55.9441, lng: -3.1618},
+	    tolerance: 500,
+	    foundMessage: "Well Done, it was Arthurs Seat"
+	  })
 	  state.game.createObjective({
-	      clue: "Go Forth!",
-	      hints: ["Over the water", "Choo Choo", "Big Red"],
-	      latLng: {lat: 56.0006, lng: -3.3884},
-	      tolerance: 500,
-	      foundMessage: "Well Done, it was the Forth Rail Bridge"
-	    })
-	
-	  localStorage.setItem('GAME', CircularJSON.stringify(state.game));
-	  var gamer = localStorage.getItem('GAME');
-	  console.log(CircularJSON.parse(gamer))
-	
-	  // state.game.currentObj.giveHint({lat: 51.4700, lng: -0.4543}, state.game.teams[0] )  
-	  // state.game.currentObj.giveHint({lat: 51.4700, lng: -0.4543}, state.game.teams[0] )  
-	  // state.game.currentObj.giveHint({lat: 51.4700, lng: -0.4543}, state.game.teams[0] )  
-	  // state.game.currentObj.giveHint({lat: -89.0700, lng: -120.4}, state.game.teams[0] )  
-	  state.game.ajax.go("GET", "/games/579fa56272b212408d172ef5")
-	
+	    clue: "Go Forth!",
+	    hints: ["Over the water", "Choo Choo", "Big Red"],
+	    latLng: {lat: 56.0006, lng: -3.3884},
+	    tolerance: 500,
+	    foundMessage: "Well Done, it was the Forth Rail Bridge"
+	  })
+	  main();
 	}
 	
+	
 	var main = function(){
+	  state.game.objectives[0].addFound(state.game.teams[1])
+	  state.game.objectives[0].addFound(state.game.teams[2])
+	  state.game.objectives[1].addFound(state.game.teams[1])
+	  state.game.objectives[2].addFound(state.game.teams[1])  
 	}
 
 /***/ },
@@ -179,9 +175,9 @@
 	      if (request.status === 200){
 	        if(request.responseText){
 	      var jsonString = request.responseText;
-	      var trimString = jsonString.substring(1,jsonString.length-1)
+	      // var trimString = jsonString.substring(1,jsonString.length-1)
 	
-	      this.response = CircularJSON.parse(trimString);
+	      this.response = CircularJSON.parse(jsonString);
 	      console.log(this.response)
 	
 	    }
--- server.js
+++ server.js
@@ -9,30 +9,57 @@ var path = require('path');
 app.use(bodyParser.json());
 app.use(express.static('client/build'));
 var url = 'mongodb://localhost:27017/cluego';
- 
+
 app.get("/", function(req,res){
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    })
+})
 
-app.post('/games', function(req,res){
-  MongoClient.connect(url, function(err, db){
-    var collection = db.collection('games');
-    collection.insert(req.body);
-    res.status(200).end();
-    db.close();
+
+// Retrieve one game object from mongodb.
+app.get('/games/:id', function(req, res) {
+  MongoClient.connect( url, function( err, db ) {
+    var collection = db.collection( 'games' );
+    var game = collection.findOne( { _id: new ObjectID(req.params.id) }, function(err, game){
+      res.json(game);
+      db.close();
+    });
+
   })
 })
 
-  app.get("/games/:id", function(req,res){
-      MongoClient.connect(url, function(err, db){
-        var collection = db.collection('games');
-        collection.find({_id: new ObjectID(req.params.id)},{_id: 0}).toArray(function(err, docs){
-          res.json(docs);
-          db.close();
-        })
+  // Index of all saved game objects.
+  app.get('/games', function(req, res) {
+    MongoClient.connect( url, function( err, db ) {
+      var collection = db.collection( 'games' );
+      collection.find({}).toArray( function( err, docs ) {
+        res.json( docs );
+        db.close();
       })
-})
+    })
+  })
 
-app.listen('3000', function(){
-  console.log("run run running on 3000");
-})
+
+  // Save one new game object to mongodb.
+  app.post('/games', function(req, res) {
+    MongoClient.connect( url, function( err, db ) {
+     var collection = db.collection( 'games' );
+     collection.insert( req.body )
+     res.status(200).end();
+     db.close();
+   })
+  });
+
+  // Delete one game object from mongodb.
+  app.delete('/games/:id', function(req, res) {
+    MongoClient.connect( url, function(err, db) {
+     var collection = db.collection( 'games' );
+     collection.remove( { _id: new ObjectID(req.params.id) } );
+     res.status(200).end();
+     db.close();
+   })
+  });
+
+
+  app.listen('3000', function(){
+    console.log("run run running on 3000");
+  })
